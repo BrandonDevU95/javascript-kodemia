02688f82d5b089f7111b3bf4c2c96e11bfbf59bf
Brandon Vargas <brandon.vargas1095@hotmail.com>
2024-04-26 20:54:21 +0000

    Refactor Koders list rendering logic in main.js

--- Practica17/js/main.js
+++ Practica17/js/main.js
@@ -1,87 +1,86 @@
+const wrapper = document.getElementById('koders-list');
 const createKoderButton = document.getElementById('create-koder');
 const db = 'https://kodemia-f0d4c-default-rtdb.firebaseio.com/koders/.json';
 
-
 const getKoders = async () => {
-	let response = await fetch(db);
-	let koders = await response.json();
+	const response = await fetch(db);
+	const koders = await response.json();
 
-	let keys = Object.keys(koders);
+	const keys = Object.keys(koders);
 
-	let kodersArray = keys.map((key) => {
-		return { ...koders[key], key };
-	});
+	const kodersArray = keys.map((key) => ({
+		...koders[key],
+		key
+	}));
 
 	return kodersArray;
 };
 
 const postKoder = async (koderObject) => {
-	let response = await fetch(db, {
+	const response = await fetch(db, {
 		method: 'POST',
 		body: JSON.stringify(koderObject),
 	});
-	let data = await response.json();
+	const data = await response.json();
 
-	if(data) printKodersList('koders-list');
-	else console.error('Error al crear el koder');
+	if (data) {
+		printKodersList();
+	} else {
+		console.error('Error al crear el koder');
+	}
 };
 
 const createKoderList = (koder) => {
 	const koderContainer = document.createElement('div');
-	koderContainer.classList.add('border', 'shadow', 'rounded', 'p-3', 'my-2');
+	koderContainer.classList.add('border', 'shadow', 'rounded', 'p-3', 'my-3');
 
 	const listKey = document.createElement('p');
 	listKey.classList.add('m-0');
 
 	const listID = document.createElement('span');
 	listID.classList.add('fw-light', 'fst-italic');
-	const spanID = document.createTextNode(koder.key);
-	listID.append(spanID);
+	listID.textContent = koder.key;
 
-	const textKey = document.createTextNode(`ID: `);
+	const textKey = document.createTextNode('ID: ');
 
 	const listFullName = document.createElement('p');
 	listFullName.classList.add('m-0');
-	const textFullName = document.createTextNode(
-		`Nombre: ${koder.name} ${koder.lastname}`
-	);
+	listFullName.textContent = `Nombre: ${koder.name} ${koder.lastname}`;
 
 	const listEmail = document.createElement('p');
 	listEmail.classList.add('m-0');
-	const textEmail = document.createTextNode(`Email: ${koder.email}`);
+	listEmail.textContent = `Email: ${koder.email}`;
 
 	listKey.append(textKey, listID);
-	listFullName.append(textFullName);
-	listEmail.append(textEmail);
-
 	koderContainer.append(listKey, listFullName, listEmail);
 
 	return koderContainer;
 };
 
-const printKodersList = async (wrapperId) => {
+const printKodersList = async () => {
 	const dataArray = await getKoders();
-	const wrapper = document.getElementById(wrapperId);
 
 	while (wrapper.firstChild) {
 		wrapper.removeChild(wrapper.firstChild);
 	}
 
-	dataArray.forEach((koder) => wrapper.append(createKoderList(koder)));
+	dataArray.forEach((koder) => {
+		const koderElement = createKoderList(koder);
+		wrapper.append(koderElement);
+	});
 };
 
 createKoderButton.addEventListener('click', (e) => {
 	e.preventDefault();
-	let koderObject = {};
 	const fields = document.querySelectorAll('#koders-form input');
+	const koderObject = {};
 
 	fields.forEach((field) => {
-		const property = field.name;
-		const value = field.value;
-		koderObject[property] = value;
+		const { name, value } = field;
+		koderObject[name] = value;
 	});
 
 	postKoder(koderObject);
 });
 
-printKodersList('koders-list');
+printKodersList();
